38397c71f56fd9007bfc52fde5eb6480754707d1
Mohit Chaudhary <mohit19214@iiitmanipur.ac.in>
2024-02-06 18:41:16 +0530

    main Commit
    Completed player controller added interaction script, added computer screen ( Place Holder for the PC model)

---
+++ SoftwareCompanySimulation/Assets/Scripts/ComputerScript/ComputerScreen.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Assets.Scripts.PlayerControllers;
+
+namespace Assets.Scripts.ComputerScript
+{
+    public class ComputerScreen: MonoBehaviour
+    {
+        [SerializeField] Camera screenCam;
+
+        private Vector3 initialPlayerCamPos;
+
+        public bool beingUsed = false;
+        public PlayerController playerController;
+        public Camera playerCam;
+        private void Start()
+        {
+            screenCam = GetComponentInChildren<Camera>();
+        }
+
+        private void Update()
+        {
+            if (!beingUsed)
+            {
+                screenCam.enabled = false;
+            }
+            else
+            {
+                UsePC();
+
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    ClosePC();
+                }
+            }
+        }
+
+        void UsePC()
+        {
+            screenCam.enabled = true;
+            playerCam.enabled = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+
+        void ClosePC()
+        {
+            beingUsed = false;
+            screenCam.enabled = false;
+            playerCam.enabled = true;
+            playerController.disableMovement = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
---
+++ SoftwareCompanySimulation/Assets/Scripts/PlayerController/InteractionController.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.ComputerScript;
+using UnityEngine;
+
+namespace Assets.Scripts.PlayerControllers
+{
+    public class InteractionController: MonoBehaviour
+    {
+        [SerializeField] PlayerController playerController;
+        private void Start()
+        {
+            
+        }
+        private void Update()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 3f))
+            {
+                if (hit.transform.CompareTag("Computer"))
+                {
+                    print("Compter in reach press E to interact");
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        ComputerScreen pcScreen = hit.transform.gameObject.GetComponent<ComputerScreen>();
+                        playerController.disableMovement = true;
+                        pcScreen.playerCam = GetComponent<Camera>();
+                        pcScreen.playerController = playerController;
+                        pcScreen.beingUsed = true;
+                    }
+                }
+            }
+        }
+    }
+}
